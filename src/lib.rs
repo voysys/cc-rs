db2889fae76d5acfd0eba094cc00cdc00cca3e3a
Jonathan Nilsson <jonathan@voysys.se>
2021-03-04 23:51:56 +0100

    Fail build on paralell build fail and clean up output

--- src/lib.rs
+++ src/lib.rs
@@ -1225,6 +1225,13 @@ impl Build {
             server.acquire_raw()?;
         }
 
+        if error.load(SeqCst) {
+            return Err(Error {
+                kind: ErrorKind::ToolExecError,
+                message: "Compilation failed".to_string(),
+            });
+        }
+
         return Ok(());
 
         /// Shared state from the parent thread to the child thread. This
@@ -1445,8 +1452,6 @@ impl Build {
 
         if !no_defaults {
             self.add_default_flags(&mut cmd, &target, &opt_level)?;
-        } else {
-            println!("Info: default compiler flags are disabled");
         }
 
         for arg in envflags {
@@ -3087,7 +3092,6 @@ fn run(cmd: &mut Command, program: &str) -> Result<(), Error> {
         }
     };
     print.join().unwrap();
-    println!("{}", status);
 
     if status.success() {
         Ok(())
@@ -3141,7 +3145,7 @@ fn run_output(cmd: &mut Command, program: &str) -> Result<Vec<u8>, Error> {
 }
 
 fn spawn(cmd: &mut Command, program: &str) -> Result<(Child, JoinHandle<()>), Error> {
-    println!("running: {:?}", cmd);
+    println!("{:?}", cmd);
 
     // Capture the standard error coming from these programs, and write it out
     // with cargo:warning= prefixes. Note that this is a bit wonky to avoid
